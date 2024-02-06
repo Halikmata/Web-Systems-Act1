dc14f290377fb88df00a8446aaf7260b0ffe3206
Jason Maverick Diokno Serencio <202180383@psu.palawan.edu.ph>
2024-02-07 01:49:31 +0800

    added new users and redirected page from sign up to log in

--- access/signup.php
+++ access/signup.php
@@ -60,7 +60,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Pass the response message to JavaScript for displaying a pop-up
     echo "<script>
             alert('" . $responseMessage . "');
-            window.location.href = 'index.php';
+            window.location.href = '../signin.php';
           </script>";
 }
 ?>
